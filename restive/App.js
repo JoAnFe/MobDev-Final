3eaa271e8c282be7bb2f2a8dddca2bc9c8baa475
J.A.F <jaf15@student.london.ac.uk>
2024-03-10 12:33:41 +0000

    large project update with pages

--- restive/App.js
+++ restive/App.js
@@ -1,20 +1,104 @@
+import * as React from 'react';
+import React, {useRef, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, useColorScheme, Animated, Dimensions, SafeAreaView } from 'react-native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const screenWidth = Dimensions.get('window').width;
+const cellHeight = screenWidth/1.4;
+
+// Home Page
+function HomePage({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Page</Text>
+      <Button
+        title="rest"
+        onPress={() => navigation.navigate('Rest')}
+      />
+      <Button
+        title="settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+// Rest Page
+function RestPage({ navigation }) {
+
+  const AnimatedText = (props) => {
+    const textOpacity = useRef(new Animated.Value(0)).current;
+
+    React.useEffect(() => {
+      Animated.timing(
+        textOpacity,
+        {
+          toValue: 1,
+          duration: 4000
+        }
+      ).start();
+    }, [textOpacity])
+
+    return (
+      <Animated.Text
+        style={{
+          ...props.style,
+          opacity: textOpacity
+        }}
+      >
+        {props.children}
+      </Animated.Text>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+     <StatusBar style="auto" />
+        <AnimatedText style={{color: "blue", fontSize: cellHeight/6}}>
+          breathe in...
+        </AnimatedText>
+
+    </View>
+  );
+}
+
+// Settings Page
+function SettingsPage() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings Page</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const scheme = useColorScheme(); // Hook to get the color scheme
+
+
+  
+  return (
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Rest" component={RestPage} />
+        <Stack.Screen name="Settings" component={SettingsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000', // force dark background
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
